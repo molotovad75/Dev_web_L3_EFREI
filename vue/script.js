c43efb8d55412dfcc0326cfb554df7ddbe7a955c
molotovad75 <adrien.lach1@gmail.com>
2022-04-25 16:34:53 +0200

    Dernière action sur le formulaire de fin de site

--- vue/script.js
+++ vue/script.js
@@ -1,3 +1,17 @@
-function afficher_titres(){
-    return "Bienvenue sur notre nouvelle plateforme d'emprunt\nIci tu peux emprunter jusqu'à 10 objets de la catégorie informatique et électronique !";
+function age_adrien(){
+    let annee=1999;
+    let mois=7;
+    let jour=2;
+    let age=0;
+    var date=new Date(); //Création de l'objet date
+    if((date.getDay<=jour || date.getDay>=jour) && date.getMonth<mois){ // Si c'est complètement inférieur au 2 juillet (le mois va de Janvier à Juin inclus)
+        age=date.getFullYear-annee-1;
+    }else if(date.getDay>=jour &&date.getMonth>=mois){
+        age=date.getFullYear-annee;
+    }else if(date.getDay<jour && date.getMonth==mois){
+        age=date.getFullYear-annee-1;
+    }
+    return age;
 }
+// window.onload=age_adrien();
+document.getElementById("age").innerHTML=age_adrien();
